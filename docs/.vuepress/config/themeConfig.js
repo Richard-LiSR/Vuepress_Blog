1886a7724993131c60c191f617926faa591e5e7e
Richard_Li <70936615+Richard-LiSR@users.noreply.github.com>
2021-09-27 10:50:09 +0800

    Update themeConfig.js

--- docs/.vuepress/config/themeConfig.js
+++ docs/.vuepress/config/themeConfig.js
@@ -10,6 +10,7 @@ module.exports = {
   sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
   logo: '/img/yhy_logo2.png', // 导航栏logo
   repo: 'https://github.com/Richard-LiSR/Vue_blog', // 导航栏右侧生成Github链接
+  search: false,
   searchMaxSuggestions: 10, // 搜索结果显示最大数
   lastUpdated: '上次更新', // 开启更新时间，并配置前缀文字   string | boolean (取值为git提交时间)
   docsDir: 'docs', // 编辑的文件夹
@@ -22,7 +23,6 @@ module.exports = {
     indexName: 'vuepress-blog',
     algoliaOptions: {
       hitsPerPage: 10,
-      facetFilters: ""
     }
   },
 
