0d4a112866ed6fb21f77e598b7e31c2ae98e8216
Richard_Li <70936615+Richard-LiSR@users.noreply.github.com>
2021-09-27 21:19:09 +0800

    Update themeConfig.js

--- docs/.vuepress/config/themeConfig.js
+++ docs/.vuepress/config/themeConfig.js
@@ -9,7 +9,7 @@ module.exports = {
   sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
   logo: '/img/yhy_logo2.png', // 导航栏logo
   repo: 'https://github.com/Richard-LiSR/Vue_blog', // 导航栏右侧生成Github链接
-  search: false,
+//   search: false,
   searchMaxSuggestions: 10, // 搜索结果显示最大数
   lastUpdated: '上次更新', // 开启更新时间，并配置前缀文字   string | boolean (取值为git提交时间)
   docsDir: 'docs', // 编辑的文件夹
@@ -17,9 +17,9 @@ module.exports = {
   editLinks: true, // 启用编辑
   editLinkText: '在Github中编辑',
   algolia: {
-    appId: "ZYGKXMP138",
-    apiKey: 'bcd57ea1b44236ecd15b49f47b5e4a7f',
-    indexName: 'vue_blog',
+    appId: "GTD22M2GZE",
+    apiKey: '0ea55b292c6633ebbf85fd8bb81c165d',
+    indexName: 'VP_blog',
     algoliaOptions: {
       facetFilters: ""
     }
